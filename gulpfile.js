da9e7abea414e3d96c9da31721d866c60c9d63cf
gabrielccraveiro <gabriel.craveiro@fcamara.com.br>
2017-11-09 14:54:41 -0200

    otimização de imagens usando imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,28 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 
 gulp.task('default', () =>
-	gulp.src('assets/img/**/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('assets/img/**/*'))
+  gulp.src('assets/img/**/*')
+  .pipe(imagemin([
+    imagemin.gifsicle({
+      interlaced: true
+    }),
+    imagemin.jpegtran({
+      progressive: true
+    }),
+    imagemin.optipng({
+      optimizationLevel: 7
+    }),
+    imagemin.svgo({
+      plugins: [{
+          removeViewBox: true
+        },
+        {
+          cleanupIDs: false
+        }
+      ]
+    })
+  ], {
+    verbose: true
+  }))
+  .pipe(gulp.dest('assets/img'))
 );
