0487a1ac17937a4a0031a9371dcbd2d29f59470c
gabrielccraveiro <gabriel.craveiro@fcamara.com.br>
2017-11-14 16:47:04 -0200

    Imagemin fotos

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,47 @@
-const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
+var gulp = require('gulp');
+var livereload = require('gulp-livereload')
+var uglify = require('gulp-uglifyjs');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
-gulp.task('default', () =>
-  gulp.src('assets/img/**/*')
-  .pipe(imagemin([
-    imagemin.gifsicle({
-      interlaced: true
-    }),
-    imagemin.jpegtran({
-      progressive: true
-    }),
-    imagemin.optipng({
-      optimizationLevel: 7
-    }),
-    imagemin.svgo({
-      plugins: [{
-          removeViewBox: true
-        },
-        {
-          cleanupIDs: false
-        }
-      ]
-    })
-  ], {
-    verbose: true
-  }))
-  .pipe(gulp.dest('assets/img'))
-);
+gulp.task('imagemin', function () {
+  return gulp.src('./assets/img/**/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{
+        removeViewBox: false
+      }],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('./assets/img/'));
+});
+
+gulp.task('sass', function () {
+  gulp.src('./sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('uglify', function () {
+  gulp.src('./lib/*.js')
+    .pipe(uglify('main.js'))
+    .pipe(gulp.dest('./js'))
+});
+
+gulp.task('watch', function () {
+  livereload.listen();
+
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./lib/*.js', ['uglify']);
+  gulp.watch(['./css/style.css', './**/*.twig', './js/*.js'], function (files) {
+    livereload.changed(files)
+  });
+});
