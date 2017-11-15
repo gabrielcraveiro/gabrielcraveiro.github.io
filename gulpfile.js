1e4eadc97619fc5b9d0cfb8af7b8e0fdc5eb6624
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2017-11-15 00:06:03 -0200

    Alteração estrutura pastas, correção na concatenação de sass

--- gulpfile.js
+++ gulpfile.js
@@ -20,28 +20,28 @@ gulp.task('imagemin', function () {
 });
 
 gulp.task('sass', function () {
-  gulp.src('./sass/**/*.scss')
+  gulp.src('./assets/scss/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: 'compressed'
     }).on('error', sass.logError))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./assets/css'));
 });
 
 gulp.task('uglify', function () {
-  gulp.src('./js/*.js')
+  gulp.src('./assets/js/*.js')
     .pipe(uglify('main.js'))
-    .pipe(gulp.dest('./js'))
+    .pipe(gulp.dest('.assets/js'))
 });
 
 gulp.task('watch', function () {
   livereload.listen();
 
-  gulp.watch('./sass/**/*.scss', ['sass']);
-  gulp.watch('./lib/*.js', ['uglify']);
-  gulp.watch(['./css/style.css', './**/*.twig', './js/*.js'], function (files) {
+  gulp.watch('./assets/scss/*.scss', ['sass']);
+  gulp.watch('./assets/js/*.js', ['uglify']);
+  gulp.watch(['./assets/css/main.css', './assets/js/*.js'], function (files) {
     livereload.changed(files)
   });
 });
